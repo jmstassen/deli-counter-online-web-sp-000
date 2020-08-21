b425de35440ab64b9e7356e0205b6d325b59961d
John Stassen <john.stassen@gmail.com>
2020-08-21 05:31:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,15 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-  end
+    count = 1
+    katz_deli.each do |name|
+      array_with_numbers << count
+      array_with_numbers << ". #{name}"
+      count += 1
+    end
+      string_with_numbers = array_with_numbers.join
+    puts "The line is currently: " + string_with_numbers
+
 end
 
 def take_a_number(katz_deli, name)
