9aa073c8e03db66b782249a66f4f501b34ec3e1d
John Stassen <john.stassen@gmail.com>
2020-08-21 05:22:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.count = 0
+  puts "The line is currently empty"
+else
+  
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving(katz_deli)
+
+end
